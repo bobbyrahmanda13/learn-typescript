ec865b365e60755891cc3e4da430bf856ee49257
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2022-12-10 10:32:55 +0700

    update typescript learn

--- TypescriptBasic/myObjects.js
+++ TypescriptBasic/myObjects.js
@@ -1,9 +1,19 @@
-var User = {
-    name: "rahman",
-    email: "rahman@gmail.com",
-    isActive: true
+"use strict";
+// const User = {
+//   name: "rahman",
+//   email: "rahman@gmail.com",
+//   isActive: true,
+// }
+// function createUser({ name: string, isPaid: boolean }) {}
+//
+// let newUser = { name: "rahman", isPaid: false, email: "jancok@mail.com" }
+//
+// createUser(newUser)
+exports.__esModule = true;
+var myUser = {
+    _id: "1234",
+    name: "r",
+    email: "rahman@mail.com",
+    isActive: false
 };
-function createUser(_a) {
-    var string = _a.name, boolean = _a.isPaid;
-}
-createUser();
+myUser.email = "lalok@gmail.com";
--- TypescriptBasic/myObjects.ts
+++ TypescriptBasic/myObjects.ts
@@ -1,14 +1,57 @@
-const User = {
-  name: "rahman",
-  email: "rahman@gmail.com",
-  isActive: true,
-}
-function createUser({ name: string, isPaid: boolean }) {}
+// const User = {
+//   name: "rahman",
+//   email: "rahman@gmail.com",
+//   isActive: true,
+// }
+// function createUser({ name: string, isPaid: boolean }) {}
+//
+// let newUser = { name: "rahman", isPaid: false, email: "jancok@mail.com" }
+//
+// createUser(newUser)
 
-createUser({ name: "rahman", isPaid: false })
+// function createCourse(): { name: string; price: number } {
+//   return { name: "vuejs", price: 6 }
+// }
 
-function createCourse(): { name: string; price: number } {
-  return {}
+// type User = {
+//   name: string
+//   email: string
+//   isActive: boolean
+// }
+//
+// function createUser(user: User): User {
+//   return { name: "", email: "", isActive: true }
+// }
+// createUser({ name: "", email: "", isActive: true })
+
+type User = {
+  readonly _id: string // tidak bisa di edit karena readonly
+  name: string
+  email: string
+  isActive: boolean
+  credcardDetails?: number // optional
 }
+
+let myUser: User = {
+  _id: "1234",
+  name: "r",
+  email: "rahman@mail.com",
+  isActive: false,
+}
+
+type cardNumber = {
+  cardnumber: string
+}
+
+type cardDate = {
+  cardDate: string
+}
+type cardDetails = cardNumber &
+  cardDate & {
+    cvv: number
+  }
+
+myUser.email = "lalok@gmail.com"
+// myUser._id = "asa"
 
 export {}
